c7065db343cf4480f1c0adc78ad824d045d4c52d
Chanoknun Choosaksilp <derpy33074@gmail.com>
2020-10-16 14:53:54 +0700

    Add Procfile

--- tust/tust/settings.py
+++ tust/tust/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tust.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'regis/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'trader/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
