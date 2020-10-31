e851ee184a8a869a5d01ba35b454b6a96108ab04
Chanoknun Choosaksilp <derpy33074@gmail.com>
2020-10-31 23:47:00 +0700

    Edit Form

--- trader/forms.py
+++ trader/forms.py
@@ -8,7 +8,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','password1','password2','first_name','last_name']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
